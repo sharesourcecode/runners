ff8932a1472bc59e7ac571111f58aa54bfc7547c
Ton Guitar <ueliton@disroot.org>
2021-12-06 15:01:03 -0300

    Update timexit.sh

--- timexit.sh
+++ timexit.sh
@@ -3,9 +3,9 @@ _timexit () {
 	for te in `seq ${@} -1 0`;
 	do
 		sleep 1
-		grep -q -o $! <<<$(ps ax) || break 2> /dev/null
+		grep -q -o $! <<<$(ps a) || break 2> /dev/null
 		[ $te == 1 ] && {
-			kill -s PIPE $! 2> /dev/null;
+			kill -9 $! 2> /dev/null;
 			echo -e "\033[31m\033[01mConnection error!\e[00m";
 			break 2> /dev/null
 		}
