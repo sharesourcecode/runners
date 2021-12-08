c07fbfc6f06253b6cc87e8f979c09fae1c54ef27
Ton Guitar <ueliton@disroot.org>
2021-12-08 14:07:18 -0300

    Update clanid.sh

--- clanid.sh
+++ clanid.sh
@@ -1,1 +1,6 @@
-404: Not Found
+# /clan id
+_clanid () {
+	( torsocks lynx -source -cfg=$TMP/$c/cfg "http://furiadetitas.net/clan" -useragent="$(shuf -n1 $TMP/$c/.ua)" >$TMP/CLD ) &
+	_timexit 20
+	CLD=$(cat $TMP/CLD | sed "s/\/clan\//\\n/g" | grep 'built\/' | awk -F\/ '{ print $1 }')
+}
